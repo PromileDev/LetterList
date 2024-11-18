f6668ae45522850a9cf20bdb97886a30e5af1b28
Oriol Carulla <04ocarulla@gmail.com>
2024-11-18 18:21:39 +0100

    home page

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
 	theme: {
 		extend: {
 			colors:{
 				"brand": "#fdba74",
 				"darkest": "#3b3732",
 				"dark": "#615951",
-				"mid": "#8db0a1",
+				"mid": "#bdb0a1",
 				"light": "#dbcfc0",
 				"lightest": "#FFFFFF"
 			}
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
