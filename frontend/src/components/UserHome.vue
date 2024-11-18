2a654c65ec5ef0659349de2788cb7d9eea317234
Oriol Carulla <04ocarulla@gmail.com>
2024-11-18 23:02:27 +0100

    userhome done

--- frontend/src/components/UserHome.vue
+++ frontend/src/components/UserHome.vue
@@ -10,6 +10,7 @@ const lastname = ref('');
 const loading = ref(true);
 const error = ref('');
 const login = ref(false);
+const dropDownVisible = ref(false);
 
 const fetchUserInfo = async () => {
   try {
@@ -36,6 +37,10 @@ const fetchUserInfo = async () => {
   }
 };
 
+const showMenu = () => {
+  dropDownVisible.value = !dropDownVisible.value;
+};
+
 onMounted(() => {
   fetchUserInfo();
 });
@@ -45,8 +50,30 @@ onMounted(() => {
     <div class="">
         <div v-if="loading">Loading...</div>
         <div v-else>
-            <div v-if="login">
-                
+            <div v-if="login" @click="showMenu" class="cursor-pointer flex gap-4 justify-center items-center " type="button">
+                <span class="text-lg">{{ name }} {{ lastname }}</span>
+                <img src="/fotos/avatar.png" class="w-10 h-10 rounded-full" alt="">
+                  <div v-if="dropDownVisible" class="absolute md:top-[70px] md:right-5 bg-white divide-y divide-gray-100 rounded-lg shadow max-w-52">
+                    <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                      <div>{{ name }} {{ lastname }}</div>
+                      <div class="font-medium truncate">{{email}}</div>
+                    </div>
+                    <ul class="py-2 text-sm text-gray-700 px-1">
+                      <li>
+                        <a href="#" class="rounded-lg block px-4 py-2 hover:bg-gray-100 ">Plans</a>
+                        
+                      </li>
+                      <li>
+                        <a href="#" class="rounded-lg block px-4 py-2 hover:bg-gray-100 ">Settings</a>
+                      </li>
+                      <li>
+                        <a href="#" class="rounded-lg block px-4 py-2 hover:bg-gray-100">Websites</a>
+                      </li>
+                    </ul>
+                    <div class="py-1 px-1">
+                      <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-red-500 hover:text-white hover:rounded-lg">Sign out</a>
+                    </div>
+                  </div>
             </div>
             <div v-else class="flex flex-row gap-4">
                 <a href="/login">Login</a>
