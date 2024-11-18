3da48ca00ec6d93323df28a7a17593606e70c2b3
Oriol Carulla <04ocarulla@gmail.com>
2024-11-18 18:37:28 +0100

    dashboard

--- frontend/src/components/UserHome.vue
+++ frontend/src/components/UserHome.vue
@@ -14,10 +14,6 @@ const login = ref(false);
 const fetchUserInfo = async () => {
   try {
     const token = localStorage.getItem('access_token');
-    if (!token) {
-      window.location.href = '/login';
-      return;
-    }
 
     const response = await axios.get('http://127.0.0.1:5000/protected', {
       headers: {
@@ -50,34 +46,14 @@ onMounted(() => {
         <div v-if="loading">Loading...</div>
         <div v-else>
             <div v-if="login">
-                <img id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" class="w-10 h-10 rounded-full cursor-pointer" src="/fotos/brand.png" alt="User dropdown">
-                <span>user</span>
-                <!-- Dropdown menu -->
-                <div id="userDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                    <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
-                    <div>Bonnie Green</div>
-                    <div class="font-medium truncate">name@flowbite.com</div>
-                    </div>
-                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                    </li>
-                    </ul>
-                    <div class="py-1">
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-                    </div>
-                </div>
+                
             </div>
-            <div v-else>
-                <span>TODO MAL</span>
+            <div v-else class="flex flex-row gap-4">
+                <a href="/login">Login</a>
+                <a href="/register">Register</a>
             </div>
         </div>
     </div>
+    
 </template>
   
