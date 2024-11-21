2eb0b359ec4ead2b55409642683294f4c853d8e3
Oriol Carulla <04ocarulla@gmail.com>
2024-11-21 10:35:40 +0100

    Update Dashboard.vue

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -51,9 +51,9 @@ onMounted(() => {
             <span v-else>{{ nameTitle }}</span>
         </div>
         <div class="mt-10 h-40 flex gap-4">
-            <a class="w-40 h-full border border-black flex items-center justify-center rounded-lg hover:shadow-lg" href=""><svg  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="none"  stroke="white"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg></a>
+            <a class="w-40 h-full border-2 border-light flex items-center justify-center rounded-lg hover:shadow-lg" href=""><svg  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="none"  stroke="white"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg></a>
             
-            <div @click="showSite(website.name)" href="" class="w-40 h-full border border-black rounded-lg hover:shadow-lg cursor-pointer" v-for="(website) in websites" :key="website.id">
+            <div @click="showSite(website.name)" href="" class="w-40 h-full border-2 border-light rounded-lg hover:shadow-lg cursor-pointer" v-for="(website) in websites" :key="website.id">
                 {{ website.name }}
             </div>
         </div>
